c78773aefb69b26f128f3e8d546eef6bb73d5e6b
Edgar Gonzalez <edgar.gonzalez@alumnos.udg.mx>
2025-02-05 00:45:37 -0600

    some smol changes

--- src/main/java/com/decajon/decajon/controllers/UserController.java
+++ src/main/java/com/decajon/decajon/controllers/UserController.java
@@ -5,8 +5,6 @@ import com.decajon.decajon.models.User;
 import com.decajon.decajon.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
